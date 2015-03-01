aa341d637d554d6bb45ef04fb5c1a7042fb5dc0f
Yunus Nedim Mehel <ynmehel@gmail.com>
2015-03-01 16:40:04 +0200

    Company identifier corrected as dilekmehel

--- Zimbo/Zimbo/Zimbo/ViewController.m
+++ Zimbo/Zimbo/Zimbo/ViewController.m
@@ -3,7 +3,7 @@
 //  Zimbo
 //
 //  Created by Yunus Nedim Mehel on 01/03/2015.
-//  Copyright (c) 2015 dilekyilmaz. All rights reserved.
+//  Copyright (c) 2015 dilekmehel. All rights reserved.
 //
 
 #import "ViewController.h"
--- Zimbo/Zimbo/ZimboTests/ZimboTests.m
+++ Zimbo/Zimbo/ZimboTests/ZimboTests.m
@@ -3,7 +3,7 @@
 //  ZimboTests
 //
 //  Created by Yunus Nedim Mehel on 01/03/2015.
-//  Copyright (c) 2015 dilekyilmaz. All rights reserved.
+//  Copyright (c) 2015 dilekmehel. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
